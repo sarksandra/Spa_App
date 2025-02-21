7dbf3bc715374cddf9b2dfdf82b1c50acd03ea36
opilane <opilane@HP-CZC4257280>
2025-02-21 11:27:06 +0200

    jgjyukmu

--- SPA_APP_2024-main/frontend/src/components/PeopleList.vue
+++ SPA_APP_2024-main/frontend/src/components/PeopleList.vue
@@ -2,21 +2,34 @@
     <div class="about">
         <h1>PeopleList</h1>
         <div class="peoplelist">
-            <DataTable :value="peoples" v-if="peoples.length > 0 ">
-                <Column field="id" header="inimese id" style="color: black; " />
-                <Column field="name" header="Inimese nimi" style="color: black; " />
-                <Column field="age" header="Inimese vanus" style="color: black; " />
-            </DataTable>      
+            <DataTable :value="peoples" v-if="peoples.length > 0">
+                <Column field="id" header="Inimese ID" style="color: black;" />
+                <Column field="name" header="Inimese Nimi" style="color: black;" />
+                <Column field="age" header="Inimese Vanus" style="color: black;" />
+            </DataTable>
             <div v-else>Inimene puuduvad</div>
         </div>
+        <div class="add-people-form">
+            <h2>Lisa Inimene</h2>
+            <form @submit.prevent="addPeople">
+                <div>
+                    <label for="name">Nimi:</label>
+                    <input v-model="newPeople.name" type="text" id="name" required />
+                </div>
+                <div>
+                    <label for="age">Vanus:</label>
+                    <input v-model="newPeople.age" type="number" id="age" required />
+                </div>
+                <button type="submit">Lisa inimene</button>
+            </form>
+        </div>
     </div>
-    
 </template>
 
 <script setup lang="ts">
     import { type People } from '@/models/people';
     import { usePeoplesStore } from "@/stores/peoplesStore";
-    import { storeToRefs } from "pinia"; 
+    import { storeToRefs } from "pinia";
     import { defineProps, onMounted, watch, ref } from "vue";
     import { useRoute } from "vue-router";
 
@@ -27,15 +40,32 @@
         }
     }, { deep: true });
 
-    
-
-    defineProps<{ title: String }>();
     const peoplesStore = usePeoplesStore();
     const { peoples } = storeToRefs(peoplesStore);
+
+    const newPeople = ref({
+        name: '',
+        age: 0,
+    });
+
+    const addPeople = async () => {
+        if (newPeople.value.name.trim() !== '' && newPeople.value.age > 0) {
+            await peoplesStore.addPeople({
+                ...newPeople.value,
+                id: 0, 
+            });
+
+            newPeople.value = { name: '', age: 0 };
+        } 
+    };
+
+    const deletePeople = async () => {
+        if()
+    }
 
     onMounted(async () => {
         peoplesStore.load();
     });
+
+
 </script>
-<style>
-</style>
